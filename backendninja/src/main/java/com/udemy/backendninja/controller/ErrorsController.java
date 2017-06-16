4b1b48a2250230206ef919dd49f4c3bca852aad4
Jose Daniel <josedaniel.juarez@beeva.com>
2017-06-16 15:28:40 +0200

    Estabilizado proyecto

--- backendninja/src/main/java/com/udemy/backendninja/controller/ErrorsController.java
+++ backendninja/src/main/java/com/udemy/backendninja/controller/ErrorsController.java
@@ -1,10 +1,16 @@
 package com.udemy.backendninja.controller;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ErrorsController {
 	
-	public String showInternal
+	public static final String ISE_VIEW = "error/500";
+	
+	@ExceptionHandler(Exception.class)
+	public String showInternalServerError(){
+		return "";
+	}
 
 }
--- backendninja/src/main/java/com/udemy/backendninja/controller/Example3Controller.java
+++ backendninja/src/main/java/com/udemy/backendninja/controller/Example3Controller.java
@@ -1,5 +1,7 @@
 package com.udemy.backendninja.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,8 @@ import com.udemy.backendninja.model.Person;
 @Controller
 @RequestMapping("/example3")
 public class Example3Controller {
+	
+	private static final Log LOGGER = LogFactory.getLog(Example3Controller.class);
 	
 	public static final String FORM_VIEW = "form";
 	public static final String RESULT_VIEW = "result";
@@ -34,14 +38,15 @@ public class Example3Controller {
 	@GetMapping("/showform")
 	public String showForm(Model model){
 		model.addAttribute("person", new Person());
-		int i = 6 / 0;
 		return FORM_VIEW;
 	}
 	
 	@PostMapping("/addperson")
 	public ModelAndView addPerson(@ModelAttribute("person") Person person){
+		LOGGER.info("METHOD: 'addPerson' -- PARAMS: '" + person + "'");
 		ModelAndView mav = new ModelAndView(RESULT_VIEW);
 		mav.addObject("person", person);
+		LOGGER.info("TEMPLATE: '"+ RESULT_VIEW +  "' -- DATA '" + person + "'");
 		
 		return mav;
 	}
