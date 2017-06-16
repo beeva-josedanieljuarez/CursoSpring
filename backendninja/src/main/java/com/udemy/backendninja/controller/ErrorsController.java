4b20ff778034fe02213e0ad4916b1f961d108896
Jose Daniel <josedaniel.juarez@beeva.com>
2017-06-16 13:39:34 +0200

    First Commit

---
+++ backendninja/src/main/java/com/udemy/backendninja/controller/ErrorsController.java
@@ -0,0 +1,10 @@
+package com.udemy.backendninja.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ErrorsController {
+	
+	public String showInternal
+
+}
